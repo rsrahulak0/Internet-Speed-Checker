1d05e2221888a9c7ddb8699a7210168c2f4a3cac
Rahul Singh <rsrahulak0@gmail.com>
2023-12-08 18:55:59 +0530

    refactoring

---
+++ app.py
@@ -0,0 +1,66 @@
+from tkinter import *
+import speedtest
+
+root = Tk()
+root.title("Internet Speed Test")
+root.geometry("360x600")
+root.resizable(False, False)
+root.configure(bg="#1a212d")
+
+def Check():
+    test = speedtest.Speedtest()
+
+    Uploading = round(test.upload()/(1024*1024), 2)
+    upload.config(text=Uploading)
+
+    downloading = round(test.download()/(1024*1024), 2)
+    download.config(text=downloading)
+    Download.config(text=downloading)
+
+
+    servernames = []
+    test.get_servers(servernames)
+    ping.config(text=test.results.ping)
+
+#icon
+# image_icon = PhotoImage(file="")
+# root.iconphoto(False, image_icon)
+
+#images
+# Top = PhotoImage(file="")
+# Label(root, image=Top, bg="#1a212d").pack()
+#
+# Main = PhotoImage(file="")
+# Label(root, image=Top, bg="#a212d").pack(pady=(40, 0))
+#
+button = PhotoImage(file="resources/start.png")
+Button=Button(root, image=button, bg="#1a212d", bd=0, activebackground="#ae212d", cursor="hand2", command=Check)
+Button.pack(pady=10, side=BOTTOM)
+
+#Label
+Label(root, text="PING", font="arial 10 bold", bg="#384056").place(x=50, y=0)
+Label(root, text="UPLOAD", font="arial 10 bold", bg="#384056").place(x=140, y=0)
+Label(root, text="DOWNLOAD", font="arial 10 bold", bg="#384056").place(x=260, y=0)
+
+Label(root, text="MS", font="arial 7 bold", bg="#384056", fg="white").place(x=60, y=80)
+Label(root, text="MBPS", font="arial 7 bold", bg="#384056", fg="white").place(x=165, y=80)
+Label(root, text="MBPS", font="arial 7 bold", bg="#384056", fg="white").place(x=275, y=80)
+
+Label(root, text="Download", font="arial 15 bold", bg="#384056", fg="white").place(x=140, y=280)
+Label(root, text="MBPS", font="arial 15 bold", bg="#384056", fg="white").place(x=155, y=380)
+
+ping = Label(root, text="00", font="arial 13 bold", bg="#384056", fg="white")
+ping.place(x=70, y=60, anchor="center")
+
+download = Label(root, text="00", font="arial 13 bold", bg="#384056", fg="white")
+download.place(x=180, y=60, anchor="center")
+
+upload = Label(root, text="00", font="arial 13 bold", bg="#384056", fg="white")
+upload.place(x=290, y=60, anchor="center")
+
+Download = Label(root, text="00", font="arial 13 bold", bg="#384056", fg="white")
+Download.place(x=185, y=350, anchor="center")
+
+
+
+root.mainloop()
